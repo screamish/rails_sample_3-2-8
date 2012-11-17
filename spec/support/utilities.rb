87d0ac77478afcfea7bb27691319765be6c6f08c
Simon Fenton <swfenton@gmail.com>
2012-11-17 01:54:13 +0000

    Better test readability by extracting matchers and helpers

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,19 @@
-def full_title(page_title)
-  base_title = "My Rails Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
+include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-error', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_success_message do |message|
+  match do |page|
+    page.should have_selector('div.alert.alert-success', text: message)
   end
 end
